2807b557af582c62861e14ef2021cf951f685df8
Luke Burns <lukemburns@gmail.com>
2022-03-22 19:59:45 -0700

    allow top-level records

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const { util, wire: { Question, types } } = require('bns')
-const base32 = require('bs32')
-const blake3 = require('blake3')
+const { util, wire: { Question, types } } = require('bns');
+const base32 = require('bs32');
+const blake3 = require('blake3');
 
 module.exports = () => ({
-  hostname: ':data.:protocol(_aliasing|aliasing).:gateway?.', 
+  hostname: ':data.:protocol(_aliasing|aliasing).:gateway?.',
   handler
-})
+});
 
 async function handler ({ data, protocol }, name, type, res, rc, ns) {
-  const dataLabels = data.split('.')
-  const hip5data = dataLabels[0]
-  
-  if (name.indexOf(protocol) > 0) {
-    return null
-  }
+  const dataLabels = data.split('.');
+  const hip5data = dataLabels[dataLabels.length - 1];
 
-  // todo: handle resolution of parent domain
+  if (name.indexOf(protocol) > 0) {
+    return null;
+  }
 
   // compute alias
-  const nameLabels = name.split('.')
-  const count = ns.name.split('.').length
-  const sldLabel = nameLabels[nameLabels.length-(count+1)]
-  const alias = util.fqdn(base32.encode(blake3.hash(sldLabel+hip5data)))
-  const nameAlias = name.replace(sldLabel+'.'+ns.name, alias)
+  const nameLabels = name.split('.');
+  const count = ns.name.split('.').length;
+  const sldLabel = nameLabels[nameLabels.length - (count + 1)];
+  const alias = util.fqdn(base32.encode(blake3.hash(sldLabel + hip5data)));
+  const nameAlias = name.replace(sldLabel + '.' + ns.name, alias);
+
+  // top-level resolution
+  if (!sldLabel) {
+    return null;
+  }
 
   // query alias
-  let response
+  let response;
   try {
-    response = await this.lookup(nameAlias, types[type])
+    response = await this.lookup(nameAlias, types[type]);
   } catch (err) {
-    return null
+    return null;
   }
   response.answer = response.answer.map(answer => {
-    answer.name = name 
-    return answer
-  })
+    answer.name = name;
+    return answer;
+  });
   response.authority = response.authority.map(answer => {
-    answer.name = name 
-    return answer
-  })
+    answer.name = name;
+    return answer;
+  });
 
-  return response
+  return response;
 }
