619066d78460ca09959ef9ee565e03c65aee18c5
Luke Burns <lukemburns@gmail.com>
2022-03-24 13:47:49 -0700

    v0.4.1 - proper DS handling, prevent cache attacks

--- index.js
+++ index.js
@@ -22,9 +22,23 @@ async function handler ({ data, protocol }, name, type, res, rc, ns) {
   const count = ns.name.split('.').length;
   const subLabels = nameLabels.slice(0, nameLabels.length - count);
   const subLabel = subLabels[subLabels.length - 1];
-  const realIndex = subLabels.findIndex(x => x[0] !== '_');
+  const firstValidIndex = subLabels.findIndex(x => x[0] !== '_');
 
-  if (realIndex < 0) {
+  if (firstValidIndex < 0) {
+    if (!rc._aliasPassthrough) {
+      rc._aliasPassthrough = true;
+      await this.middleware(rc);
+      const res = rc.res;
+
+      // strict answers only
+      // todo: improve filtering to prevent cache takeovers by TLD owner
+      res.answer = res.answer.filter(rec => {
+        return rec.name === name;
+      });
+
+      return rc.res;
+    }
+
     return null;
   }
 
@@ -35,17 +49,31 @@ async function handler ({ data, protocol }, name, type, res, rc, ns) {
   if (res.authority.filter(x => x.name.indexOf(nameAlias) > -1).length > 0) {
     return empty.resolve(name, types[type]);
   } else {
-    const res = await this.stub.lookup(nameAlias, types.A);
-    res.answer = [];
+    const res = await this.stub.lookup(nameAlias, types[type]);
+
     res.question = rc.res.question;
+
+    res.answer = res.answer.map(answer => {
+      answer.name = answer.name.replace(alias, domain);
+      return answer;
+    });
+
     res.authority = res.authority.map(answer => {
       answer.name = answer.name.replace(alias, domain);
       return answer;
     });
+
     res.additional = res.additional.map(answer => {
       answer.name = answer.name.replace(alias, domain);
       return answer;
     });
+
+    // answer with DS record
+    if (type === 'DS') {
+      return res;
+    }
+
+    // otherwise just pass along referral
     rc.res = res;
     return null;
   }
